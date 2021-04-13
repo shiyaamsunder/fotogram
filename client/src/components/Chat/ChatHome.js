f31c119f78f8c1915b9a966cdae4df61ebe0d273
shiyaamsunder <63956643+shiyaamsunder@users.noreply.github.com>
2021-04-13 23:19:40 +0530

    removed log

--- client/src/components/Chat/ChatHome.js
+++ client/src/components/Chat/ChatHome.js
@@ -5,7 +5,6 @@ const ChatHome = () => {
 		<div className="mt-14">
 			<h1 className="text-2xl font-bold text-purple-500 text-center">
 				Chat feature coming soon
-				{console.log(process.env.REACT_APP_ENV)}
 			</h1>
 		</div>
 	);
