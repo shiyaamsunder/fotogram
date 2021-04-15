33bdc7695540c4d7ff709e93e0267ad9b82b9f50
shiyaamsunder <63956643+shiyaamsunder@users.noreply.github.com>
2021-04-15 17:33:36 +0530

    ws fix

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const io = require("socket.io")(server, {
 	cors: {
 		origin:
 			process.env.NODE_ENV === "production"
-				? "http://localhost:8000"
+				? `http://localhost:${process.env.PORT}`
 				: "http://localhost:3000",
 	},
 });
